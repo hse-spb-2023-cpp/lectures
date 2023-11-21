e20a207b8b0ff02f66e90c657d59cb35e7f00494
Egor Suvorov <egor.suvorov@gmail.com>
2023-11-21 15:13:13 +0200

    10-231121: add after class

---
+++ 10-231121/01-rule-of-five/13-deleted.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+struct MoveOnly {
+    MoveOnly() {}
+
+    // Alternative: make both `private`.
+    MoveOnly(const MoveOnly &) = delete; /*{
+        assert(false);
+        std::cout << "copy ctor\n";
+    }*/
+    MoveOnly &operator=(const MoveOnly &) = delete;/*{
+        std::cout << "copy assign\n";
+        return *this;
+    }*/
+
+    MoveOnly(MoveOnly &&) {
+        std::cout << "move ctor\n";
+    }
+    MoveOnly &operator=(MoveOnly &&) {
+        std::cout << "move assign\n";
+        return *this;
+    }
+};
+
+int main() {
+    MoveOnly a;
+    MoveOnly b;
+    a = b;
+    a = std::move(b);
+}
